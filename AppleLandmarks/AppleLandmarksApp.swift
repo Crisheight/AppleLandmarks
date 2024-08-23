5392a2562acdff8bc2d5f4a0c8b635a186d3b265
Cris <crisheight@gmail.com>
2024-08-23 01:03:41 -0500

    Initial Commit

---
+++ AppleLandmarks/AppleLandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleLandmarksApp.swift
+//  AppleLandmarks
+//
+//  Created by Cris Rosa on 8/23/24.
+//
+
+import SwiftUI
+
+@main
+struct AppleLandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppleLandmarksTests/AppleLandmarksTests.swift
@@ -0,0 +1,17 @@
+//
+//  AppleLandmarksTests.swift
+//  AppleLandmarksTests
+//
+//  Created by Cris Rosa on 8/23/24.
+//
+
+import Testing
+@testable import AppleLandmarks
+
+struct AppleLandmarksTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
