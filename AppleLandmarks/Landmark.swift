081c39bbaeb6d83247a8db55ad56059a235da9cd
Cris <crisheight@gmail.com>
2024-12-15 15:28:26 -0600

    removed

--- AppleLandmarks/Landmark.swift
+++ AppleLandmarks/Landmark.swift
@@ -6,6 +6,8 @@
 //
 
 import Foundation
+import SwiftUI
+import CoreLocation
 
 
 struct Landmark: Hashable, Codable {
@@ -14,4 +16,21 @@ struct Landmark: Hashable, Codable {
     var park: String
     var state: String
     var description: String
+    
+    private var imageName: String
+    var image: Image {
+        Image(imageName)
+    }
+    
+    private var coordinates: Coordinates
+    var locationCoordinate: CLLocationCoordinate2D {
+        .init(latitude: coordinates.latitude,
+              longitude: coordinates.longitude)
+    }
+    
+    struct Coordinates: Hashable, Codable {
+        var latitude: Double
+        var longitude: Double
+    }
+    
 }
