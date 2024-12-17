0f65f1c9dbf9da9500f46b090754bbacc2c85c82
Cris <crisheight@gmail.com>
2024-12-16 21:34:54 -0600

    LandmarkRow class provides a flexible object view for landmark names.

--- AppleLandmarks/Views/LandmarkRow.swift
+++ AppleLandmarks/Views/LandmarkRow.swift
@@ -5,4 +5,16 @@
 //  Created by Cris Rosa on 12/16/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct LandmarkRow: View {
+    var landmark: Landmark
+    
+    var body: some View {
+        Text(landmark.name)
+    }
+}
+
+#Preview {
+    LandmarkRow(landmark: landmarks[0])
+}
