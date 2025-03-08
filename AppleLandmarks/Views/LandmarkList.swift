dbcb5c156944c68e496bb52f7bf6749706640487
Cris <crisheight@gmail.com>
2025-03-07 22:34:31 -0600

    Create a struct to display the desired landmarks in a row

---
+++ AppleLandmarks/Views/LandmarkList.swift
@@ -0,0 +1,21 @@
+//
+//  LandmarkList.swift
+//  AppleLandmarks
+//
+//  Created by Cris Rosa on 3/7/25.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List {
+            LandmarkRow(landmark: landmarks[0])
+            LandmarkRow(landmark: landmarks[1])
+        }
+    }
+}
+
+#Preview {
+    LandmarkList()
+}
