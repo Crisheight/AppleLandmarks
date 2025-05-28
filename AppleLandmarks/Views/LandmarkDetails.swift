64bd456e8b5a516674c3a611d378e180de7c42b0
Cris <crisheight@gmail.com>
2025-05-28 11:48:23 -0400

    add new landmark class with preview

---
+++ AppleLandmarks/Views/LandmarkDetails.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarkDetails.swift
+//  AppleLandmarks
+//
+//  Created by Cris Rosa on 5/28/25.
+//
+import SwiftUI
+
+struct LandmarkDetails: View {
+    var body: some View {
+        Text("Landmark Details")
+    }
+}
+
+#Preview {
+    LandmarkDetails()
+}
