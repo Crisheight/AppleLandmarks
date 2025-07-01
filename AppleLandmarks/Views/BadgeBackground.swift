d054453ffc136a1ab1e87618d8a295154ee33810
Cris <crisheight@gmail.com>
2025-07-01 19:13:42 -0400

    Create a new view for the hexagon. Define and implement the initial hexagon.

---
+++ AppleLandmarks/Views/BadgeBackground.swift
@@ -0,0 +1,18 @@
+//
+//  BadgeBackground.swift
+//  AppleLandmarks
+//
+//  Created by Cris Rosa on 7/1/25.
+//
+
+import SwiftUI
+
+struct BadgeBackground: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BadgeBackground()
+}
