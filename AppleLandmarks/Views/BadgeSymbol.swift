1c8cca23ff3f618f914b5b9e30e8eaab44df7583
Cris <crisheight@gmail.com>
2025-07-03 18:25:36 -0400

    Create bottom half of symbol, add color. Symbol is complete.

--- AppleLandmarks/Views/BadgeSymbol.swift
+++ AppleLandmarks/Views/BadgeSymbol.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 
 struct BadgeSymbol: View {
+    static let symbolColor = Color(red: 79.0 / 255, green: 79.0 / 255, blue: 191.0 / 255)
+    
     var body: some View {
         GeometryReader { geometry in
             Path { path in
@@ -18,8 +20,8 @@ struct BadgeSymbol: View {
                 let middle = width * 0.5
                 let topWidth = width * 0.226
                 let topHeight = height * 0.488
-
-
+                
+                
                 path.addLines([
                     CGPoint(x: middle, y: spacing),
                     CGPoint(x: middle - topWidth, y: topHeight - spacing),
@@ -27,7 +29,17 @@ struct BadgeSymbol: View {
                     CGPoint(x: middle + topWidth, y: topHeight - spacing),
                     CGPoint(x: middle, y: spacing)
                 ])
+                
+                path.move(to: CGPoint(x: middle, y: topHeight / 2 + spacing * 3))
+                path.addLines([
+                    CGPoint(x: middle - topWidth, y: topHeight + spacing),
+                    CGPoint(x: spacing, y: height - spacing),
+                    CGPoint(x: width - spacing, y: height - spacing),
+                    CGPoint(x: middle + topWidth, y: topHeight + spacing),
+                    CGPoint(x: middle, y: topHeight / 2 + spacing * 3)
+                ])
             }
+            .fill(Self.symbolColor)
         }
     }
 }
