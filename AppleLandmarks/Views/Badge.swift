9c4771bcf669d8121e4da1e86a67a789dbc2f989
Cris <crisheight@gmail.com>
2025-07-14 15:59:20 -0400

    Init

---
+++ AppleLandmarks/Views/Badge.swift
@@ -0,0 +1,18 @@
+//
+//  Badge.swift
+//  AppleLandmarks
+//
+//  Created by Cris Rosa on 7/14/25.
+//
+
+import SwiftUI
+
+struct Badge: View {
+    var body: some View {
+        BadgeBackground()
+    }
+}
+
+#Preview {
+    Badge()
+}
