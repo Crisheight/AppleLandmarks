f8645027ebc31035cf6de2c0c5d0f72e5d88d524
Cris <crisheight@gmail.com>
2025-07-14 16:41:29 -0400

    Create a Badges directory and move badge-related code/assets into it.

---
+++ AppleLandmarks/Views/Badges/RotatedBadgeSymbol.swift
@@ -0,0 +1,23 @@
+//
+//  RotatedBadgeSymbol.swift
+//  AppleLandmarks
+//
+//  Created by Cris Rosa on 7/3/25.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+
+#Preview {
+    RotatedBadgeSymbol(angle: Angle(degrees: 5))
+}
