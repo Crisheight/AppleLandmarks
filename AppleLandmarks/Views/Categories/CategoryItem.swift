a2a23b81399ea6bb137c785f86eb4de43f82747c
Cris <crisheight@gmail.com>
2025-08-07 16:53:16 -0400

    Displays a singular landmark as a view

---
+++ AppleLandmarks/Views/Categories/CategoryItem.swift
@@ -0,0 +1,18 @@
+//
+//  CategoryItem.swift
+//  AppleLandmarks
+//
+//  Created by Cris Rosa on 8/7/25.
+//
+
+import SwiftUI
+
+struct CategoryItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CategoryItem()
+}
