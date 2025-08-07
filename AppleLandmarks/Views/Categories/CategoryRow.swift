e78864947c1ee956962b9028aef5838cacf4fec0
Cris <crisheight@gmail.com>
2025-08-07 16:46:19 -0400

    Use VStack and Hstack to split the category and items in said category across the total view

---
+++ AppleLandmarks/Views/Categories/CategoryRow.swift
@@ -0,0 +1,18 @@
+//
+//  CategoryRow.swift
+//  AppleLandmarks
+//
+//  Created by Cris Rosa on 8/7/25.
+//
+
+import SwiftUI
+
+struct CategoryRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CategoryRow()
+}
